135b72ec3c9ad52940b1bc888827f684985853f6
ahridesu <ariana.gg@ua.pt>
2025-03-11 19:54:13 +0000

    workaround

---
+++ tf/locals.tf
@@ -0,0 +1,11 @@
+locals {
+    content_types = {
+    css  = "text/css"
+    html = "text/html"
+    js   = "application/javascript"
+    woff = "font/woff"
+    png  = "image/png"
+    pdf  = "application/pdf"
+    txt  = "text/plain"
+  }
+}
--- tf/s3.tf
+++ tf/s3.tf
@@ -57,6 +57,8 @@ resource "aws_s3_object" "index" {
   bucket       = aws_s3_bucket.website.id
   key          = each.value  
   source       = "../website/${each.value}"
-  content_type = mime_type("../website/${each.value}")
-
+  content_type     = lookup(local.content_types, element(split(".", each.value), length(split(".", each.value)) - 1), "text/plain")
+  content_encoding = "utf-8"
 }
+
+

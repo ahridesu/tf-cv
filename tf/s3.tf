59e0a77e3ef53bad3decfdedf296dc775e0f08d6
ahridesu <ariana.gg@ua.pt>
2025-03-11 17:03:24 +0000

    fix

--- tf/s3.tf
+++ tf/s3.tf
@@ -50,11 +50,10 @@ resource "aws_s3_bucket_policy" "website" {
       "Action": [
         "s3:PutObject",
         "s3:GetObject",
-        "s3:DeleteObject",
+        "s3:DeleteObject"
       ],
       "Resource": "arn:aws:s3:::cv-website-project/*"
-    },
-
+    }
   ]
 }
 POLICY

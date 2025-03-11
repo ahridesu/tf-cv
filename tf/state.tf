8981fae2c9956577b49f4170559354c9bfb3a68b
ahridesu <ariana.gg@ua.pt>
2025-03-11 15:53:30 +0000

    adding state bucket

--- tf/state.tf
+++ tf/state.tf
@@ -1,3 +1,37 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "infra-terraform-state-bucket"
+
+  lifecycle {
+    prevent_destroy = true 
+  }
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Production"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
 terraform {
   backend "s3" {
     bucket         = "infra-terraform-state-bucket"
